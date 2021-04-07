fc013e2f55335d97be4653f0283872c5f14cfe22
Erik Ford <ekford18@gmail.com>
2021-04-07 15:57:54 -0700

    first commit, drag and drop working

---
+++ src/components/Card.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+function Card (props) {
+console.log(props)
+    const dragStart = e => {
+        const target = e.target;
+        e.dataTransfer.setData('card_id', target.id)
+
+        setTimeout(() => {
+            target.style.display = "none";
+        }, 0);
+    }
+
+    const dragOver = e => {
+        e.stopPropagation();
+    }
+
+    return (
+        <div
+            id={props.id}
+            className={props.className}
+            draggable="true"
+            onDragStart={dragStart}
+            onDragOver={dragOver}
+        >
+            { props.children }
+        </div>
+    )
+}
+
+export default Card
