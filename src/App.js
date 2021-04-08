a868fcceff6837cec4c1cb317387c4d93927d62b
Erik Ford <ekford18@gmail.com>
2021-04-08 14:28:04 -0700

    added basic cs for sizing, working on more pieces

--- src/App.js
+++ src/App.js
@@ -8,13 +8,11 @@ function App() {
       <main className="flexbox">
         <Board id="board-1" className="board">
           <Card id="card-1" className="card" draggable="true">
-            <p>Card One</p>
           </Card>
         </Board>
 
         <Board id="board-2" className="board">
           <Card id="card-2" className="card" draggable="true">
-            <p>Card Two</p>
           </Card>
         </Board>
       </main>
--- src/components/Card.js
+++ src/components/Card.js
@@ -24,6 +24,7 @@ console.log(props)
             onDragOver={dragOver}
         >
             { props.children }
+            Player
         </div>
     )
 }
